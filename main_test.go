c38a6850fbaaa3df25141f80e80219cd6bc6d788
pandudw <nasrullah.pandud1@gmail.com>
2023-08-06 00:17:27 +0700

    add unit test golang

--- main_test.go
+++ main_test.go
@@ -1,14 +1,11 @@
-// main.go
-
 package main
 
-import "fmt"
+import "testing"
 
-func Add(a, b int) int {
-	return a + b
-}
-
-func main() {
-	result := Add(3, 5)
-	fmt.Println("Result:", result)
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+	}
 }
