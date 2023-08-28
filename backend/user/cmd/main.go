7088a9a041e9b934e899af9a6e6319bb82620cc0
digitech-i <141510787+digitech-i@users.noreply.github.com>
2023-08-28 16:37:52 +0700

    revamp code to use sql db

--- backend/user/cmd/main.go
+++ backend/user/cmd/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"myhealth/backend/user/internal/handler"
+	"myhealth/backend/user/internal/repository"
 	"myhealth/backend/user/internal/service"
 	"net/http"
 )
 
 func main() {
-	userService := service.NewUserService()
+	// Database connection setup
+	db, err := sql.Open("mysql", "username:password@tcp(host:port)/database")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Repository
+	userRepository := repository.NewUserRepository(db)
+
+	// Service
+	userService := service.NewUserService(userRepository)
+
+	// Handler
 	userHandler := handler.NewUserHandler(userService)
 
-	http.HandleFunc("/users", userHandler.GetAllUsers)
-	http.HandleFunc("/users/add", userHandler.AddUser)
+	http.HandleFunc("/createUser", userHandler.CreateUser)
+	http.HandleFunc("/getUsers", userHandler.GetUsers)
 
 	port := 8080
-	fmt.Printf("Server started on port %d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Server listening on port %d...\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
